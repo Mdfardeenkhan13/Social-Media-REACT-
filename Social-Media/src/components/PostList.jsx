b07f07fac645d105b3a25635ac19430e7bcd279e
Mdfardeenkhan3 <mdfardeenkhan768@gmail.com>
2025-05-17 01:12:42 +0530

    useEffect

--- Social-Media/src/components/PostList.jsx
+++ Social-Media/src/components/PostList.jsx
@@ -1,19 +1,16 @@
 import Post from "./Post";
 import { PostList as PostListData } from './../store/post-list-store';
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import WelcomeMessage from "./WelcomeMessage";
 
 const PostList = () => {
     const {postList, addInitialPosts} = useContext(PostListData);
 
-    const [dataFetching, setDataFetching] = useState(false);
-
-    if(!dataFetching) {
+    useEffect (() => {
         fetch('https://dummyjson.com/posts')
             .then(res => res.json())
             .then(data => addInitialPosts(data.posts));
-            setDataFetching(true);
-    }
+    }, [])
 
     return (
         <>
